7ca25dfe7a2105696d4f63b8054143d6ad334e67
anh.tran(c0720i1) <69283798+theanh111@users.noreply.github.com>
2020-09-11 10:32:19 +0700

    thuc hanh Dao nguoc cac phan tu cua mang

---
+++ src/ReverseArrayElement.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class ReverseArrayElement {
+    public static void main(String[] args) {
+        int size;
+        int[] array;
+        Scanner input = new Scanner(System.in);
+        do {
+            System.out.print("Enter a size: ");
+            size = input.nextInt();
+            if (size > 20) {
+                System.out.println("Size does not exceed 20");
+            } else {
+                array = new int[size];
+                int i = 0;
+                while (i < array.length) {
+                    System.out.print("Enter element " + (i + 1) + ": ");
+                    array[i] = input.nextInt();
+                    i++;
+                }
+                System.out.printf("%-20s%s", "Elements in array: ", "");
+                for (int j = 0; j < array.length; j++) {
+                    System.out.print(array[j] + "\t");
+                }
+
+                for (int j = 0; j < array.length / 2; j++) {
+                    int temp = array[j];
+                    array[j] = array[size - 1 - j];
+                    array[size - 1 - j] = temp;
+                }
+
+                System.out.println();
+                System.out.printf("%-20s%s", "Reversed array: ", "");
+                for (int j = 0; j < array.length; j++) {
+                    System.out.print(array[j] + "\t");
+
+                }
+            }
+        }
+        while (size > 20);
+    }
+}
